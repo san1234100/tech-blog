bceaff91904070c057c190fc58115a2dbdf0e934
CHANDRU U <sanjeevchandru1234@gmail.com>
2024-03-13 13:34:58 +0530

    design: nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Nav from "./components/Nav"
 import TheHeader from "./components/TheHeader"
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
     <>
      <TheHeader/>
      <div className="h-0.5 bg-lightBlue mx-20"></div>
+     <Nav/>
     </>
   )
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+const Nav = () => {
+  return (
+    <ul className="flex items-center max-w-fit pb-5 mx-auto space-x-10 my-5 select-none text-white font-medium">
+      <li className="cursor-pointer hover:text-lightBlue">Home</li>
+      <li className="cursor-pointer hover:text-lightBlue">Posts</li>
+      <li className="cursor-pointer hover:text-lightBlue">About</li>
+      <li className="cursor-pointer hover:text-lightBlue">Contact</li>
+    </ul>
+  );
+};
+
+export default Nav;
