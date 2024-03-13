09a997bc62603511a21a4307a17423c4695ca85c
CHANDRU U <sanjeevchandru1234@gmail.com>
2024-03-13 14:03:28 +0530

    design: bg-hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Hero from "./components/Hero"
 import Nav from "./components/Nav"
 import TheHeader from "./components/TheHeader"
 
@@ -7,6 +8,7 @@ function App() {
      <TheHeader/>
      <div className="h-0.5 bg-lightBlue mx-20"></div>
      <Nav/>
+     <Hero/>
     </>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+const Hero = () => {
+    return ( 
+        <section className="flex space-x-10 items-center">
+              <img src="./src/assets/images/web-development1.png" className="ms-10 w-[50%] rounded" alt="" />
+              <div className="text-white">
+                <h1 className="text-4xl font-semibold">Welcome, to <span className="text-midyellow">Tech Blog</span></h1>
+                <p className="py-3 w-[540px]">
+                Dive into a world of HTML, CSS, JavaScript, and beyond as we explore the latest trends and techniques. Whether you're a beginner or a seasoned pro, we've got something for everyone, from tutorials to industry insights. Join our community of developers, designers, and enthusiasts as we unravel the mysteries of the web. 
+                </p>
+              </div>
+        </section>
+     );
+}
+ 
+export default Hero;
